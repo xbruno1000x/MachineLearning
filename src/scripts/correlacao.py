66e4e05a4dfa67c117b5d817ea1b3fcfe407f598
xBruno1000x <fariabruno10@gmail.com>
2026-03-27 13:23:45 -0300

    :sparkles: feat: Implementa o script do Naive-Bayes

--- src/scripts/correlacao.py
+++ src/scripts/correlacao.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 ax = sns.heatmap(
     correlacao,
-    vmin=-1, vmax=1, center=0
+    vmin=-1, vmax=1, center=0,
     cmap=sns.diverging_palette(20, 220, n=200),
     square=True,
     xticklabels=True,
---
+++ src/scripts/naive-bayes.py
@@ -0,0 +1,44 @@
+# %%
+from pathlib import Path
+import pandas as pd
+import sys
+
+SRC_DIR = Path(__file__).resolve().parents[1]
+if str(SRC_DIR) not in sys.path:
+	sys.path.insert(0, str(SRC_DIR))
+
+csv_path = Path('../data/loan_risk_balanceado.csv')
+df = pd.read_csv(csv_path)
+df.head(5)
+
+# %%
+df.value_counts('LoanApproved')
+
+# %%
+categoricasOrdinais = []
+
+# %%
+configs = {
+	"nome_arquivo": Path('../data/loan_risk_balanceado.csv'),
+	"cols_dummy": ['City', 'Gender', 'Education', 'EmploymentType'],
+	"cols_categoria_ordinal": categoricasOrdinais,
+	"random_state": 42,
+}
+
+# %%
+from models.classificador import Classificador
+from models.pre_processador import PreProcessador
+from models.metrificador import Metrificador
+
+pre_processador = PreProcessador(configs)
+classificador = Classificador(pre_processador)
+
+classificador.NaiveBayes()
+
+metrificador = Metrificador(classificador)
+acuracia = metrificador.acuracia()
+matriz_confusao = metrificador.matrizConfusao()
+
+# %%
+print(f"Acuracia: {acuracia}")
+print(f"Matriz de Confusao:\n{matriz_confusao}")
